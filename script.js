11c7da2aafd1ab5a95d4373cc514267f397fc281
bWX1149360 <brdc.ccxlv1@h-partners.com>
2022-08-30 17:01:00 -0300

    Timer feature

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
-import { Timer } from 'easytimer.js'
-
-const timer = new Timer()
-
-$('.startButton').click(function () {
-    timer.start({precision: "seconds"});
-});
-
-$('.pauseButton').click(function () {
-    timer.pause();
-});
-
-$('.resetButton').click(function () {
-    timer.reset();
-});
-
-timer.addEventListener('secondsUpdated', function (e) {
-    $('.value-container').html(timer.getTimeValues().toString());
-});
-
-timer.addEventListener('started', function (e) {
-    $('.value-container').html(timer.getTimeValues().toString());
-});
-
-timer.addEventListener('reset', function (e) {
-    $('.value-container').html(timer.getTimeValues().toString());
-});
+// import { Timer } from 'easytimer.js'
+//
+// const timer = new Timer()
+//
+// $('.startButton').click(function () {
+//     timer.start({precision: "seconds"});
+// });
+//
+// $('.pauseButton').click(function () {
+//     timer.pause();
+// });
+//
+// $('.resetButton').click(function () {
+//     timer.reset();
+// });
+//
+// timer.addEventListener('secondsUpdated', function (e) {
+//     $('.value-container').html(timer.getTimeValues().toString());
+// });
+//
+// timer.addEventListener('started', function (e) {
+//     $('.value-container').html(timer.getTimeValues().toString());
+// });
+//
+// timer.addEventListener('reset', function (e) {
+//     $('.value-container').html(timer.getTimeValues().toString());
+// });
 
 
 let progressBar = document.querySelector(".circular-progress");
+let valueContainer = document.querySelector(".value-container")
 
 let progressValue = 0;
 let progressEndValue = 100;
 
+let hour = 0
+let minute = 0
+let second = 0
+
 progressBar.style.background = `conic-gradient(
-      #dc3545 ${progressValue * 3.6}deg,
-      #eeeeee ${progressValue * 3.6}deg
+      #dc3545 ${progressValue}deg,
+      #eeeeee ${progressValue}deg
   )`;
 
-// let progress = setInterval(() => {
-//   progressValue++;
-//   valueContainer.textContent = `${progressValue}%`;
-//   progressBar.style.background = `conic-gradient(
-//       #dc3545 ${progressValue * 3.6}deg,
-//       #eeeeee ${progressValue * 3.6}deg
-//   )`;
-//   if (progressValue === progressEndValue) {
-//     clearInterval(progress);
-//   }
-// }, 1000);
+let progress = setInterval(() => {
+    progressValue++
+    second++
+  if (second === 60){
+      second = 0
+      minute++
+  }
+
+  if (minute === 60){
+      minute = 0
+      hour++
+  }
+
+  valueContainer.textContent = hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0')
+  progressBar.style.background = `conic-gradient(
+      #dc3545 ${progressValue / 18}deg,
+      #eeeeee ${progressValue / 18}deg
+  )`;
+
+  if ((progressValue / 18) === progressEndValue) {
+    clearInterval(progress);
+  }
+}, 1000);
