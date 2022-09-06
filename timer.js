dabb2c451d642aafd4ad2d07ccaa49f0d1cfb490
bWX1149360 <brdc.ccxlv1@h-partners.com>
2022-09-06 16:39:13 -0300

    Desing changes

--- timer.js
+++ timer.js
@@ -4,6 +4,7 @@ let valueContainer = document.querySelector(".value-container");
 let progressValue = 0;
 let progressEndValue = 100;
 let lastId = 0;
+let canClick = true
 
 let hour = 0;
 let minute = 0;
@@ -11,7 +12,7 @@ let second = 0;
 
 progressBar.style.background = `conic-gradient(
       #dc3545 ${progressValue}deg,
-      #eeeeee ${progressValue}deg
+      #dedede ${progressValue}deg
   )`;
 
 let progress = null;
@@ -23,44 +24,66 @@ if (!lastId){
 }
 
 function start(){
-    progress = setInterval(() => {
-      progressValue++;
-      second++;
+    if (canClick){
+        canClick = false
+        progress = setInterval(() => {
+        progressValue++;
+        second++;
 
-      if (second === 60){
-          second = 0;
-          minute++;
-      }
+            if (second === 60){
+                second = 0;
+                minute++;
+            }
 
-      if (minute === 60){
-          minute = 0;
-          hour++;
-      }
+            if (minute === 60){
+                minute = 0;
+                hour++;
+            }
 
-      valueContainer.textContent = hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0');
-      progressBar.style.background = `conic-gradient(
-          #dc3545 ${progressValue * 3.6}deg,
-          #eeeeee ${progressValue * 3.6}deg
-      )`;
+            valueContainer.textContent = hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0');
+            progressBar.style.background = `conic-gradient(
+                #dc3545 ${progressValue / 1.66666667}deg,
+                #dedede ${progressValue / 1.66666667}deg
+            )`;
 
-      if ((progressValue * 3.6) === progressEndValue) {
-        clearInterval(progress);
-      }
-    }, 1000);
+            if ((progressValue / 6) === progressEndValue) {
+              save()
+            }
+        }, 1000);
+    }
 }
 
 function pause(){
     clearInterval(progress);
+    canClick = true
 }
 
 function save(){
-    clearInterval(progress);
-    let auxTime = hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0');
-    if (localStorage.getItem(lastId) !== auxTime){
-        lastId++
-        localStorage.setItem('ID', lastId)
-        localStorage.setItem(lastId.toString(), hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0'))
+    if (!canClick) {
+        clearInterval(progress);
+        let auxTime = hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0');
+        if (localStorage.getItem(lastId) !== auxTime){
+            lastId++
+            localStorage.setItem('ID', lastId)
+            localStorage.setItem(lastId.toString(), hour.toString().padStart(2, '0') + ':' + minute.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0'))
+        }
+        progressValue = 0;
+        second = 0;
+
+        canClick = true;
+        notify().then(r => {
+            document.getElementById('notification').textContent = '';
+        })
     }
-    progressValue = 0;
-    second = 0;
+}
+
+async function notify(){
+    document.getElementById('notification').textContent = 'Saved successfully';
+    await delay(2000)
+}
+
+function delay(ms) {
+  return new Promise((resolve, _reject) => {
+    setTimeout(resolve, ms);
+  });
 }
